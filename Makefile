31b1ce012eb0d31c05f12545fdfabab5d696d063
kanemoda <efedenizbaglar45@gmail.com>
2025-02-09 21:17:02 +0300

    Make definitions in header file

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	gcc engin.c -o engin
---
+++ engin.c
@@ -0,0 +1,8 @@
+#include "therook.h"
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+
+  printf("Compiled Sucessfully");
+  return 0;
+}
